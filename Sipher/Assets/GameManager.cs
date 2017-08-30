43053bd5d463fc09c17ef8d52b3bca21fe5f64e7
Usman Ali <imusmanali@hotmail.com>
2017-08-30 20:13:40 +0500

    functionality Implemented

---
+++ Sipher/Assets/GameManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    //initial gameSetup
+    public GameObject mainLevel;
+    public GameObject[] usedPoligons;
+    public GameObject[] unusedPoligons;
+    public int[] rotatonAngles;
+    public int[] levelOrentationn = new int[] { 1,-1 };
+
+
+    //conditoin check
+    public GameObject[] connectors;
+    public bool condition = false;
+
+
+    //singleton
+    private static GameManager instance = null;
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+
+
+    private void Awake()
+    {
+        
+        // if the singleton hasn't been initialized yet
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+        InitialConditions();
+    }
+    // Use this for initialization
+    void Start()
+    {
+        connectors = GameObject.FindGameObjectsWithTag("Connecter");
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //if (Input.GetMouseButtonUp(0))
+        //{
+        //    check();
+        //}
+    }
+
+
+
+    public void check()
+    {
+        if (condition == true)
+        {
+            return;
+        }
+
+
+        Debug.Log("Test Complete");
+        foreach (GameObject connector in connectors)
+        {
+            if (connector.GetComponent<CONNECTER>().connected == false)
+            {
+                condition = false;
+                return;
+            }
+
+        }
+
+        condition = true;
+    }
+
+    void InitialConditions()
+    {
+        int i = Random.Range(0, levelOrentationn.Length);
+        int j = Random.Range(0, levelOrentationn.Length);
+
+        Vector3 newScale=mainLevel.transform.localScale;
+        newScale.x *= levelOrentationn[i];
+        newScale.y *= levelOrentationn[j];
+        mainLevel.transform.localScale = newScale;
+
+
+        foreach (GameObject poligon in usedPoligons)
+        {
+            int k = Random.Range(0, rotatonAngles.Length);
+
+           poligon.transform.Rotate(new Vector3(0, 0, 1), rotatonAngles[k]);
+        }
+    }
+}
---
+++ Sipher/Assets/Poligon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poligon : MonoBehaviour
+{
+    float Speed = 1f;
+    public Transform fromRotation;
+    public Transform toRotation;
+    int angle = -60;
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Rotate()
+    {
+        transform.Rotate(new Vector3(0, 0, -1) * Speed * Time.deltaTime, 60);
+    }
+}
